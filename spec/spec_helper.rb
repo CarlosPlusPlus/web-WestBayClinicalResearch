02ad1beb3454e03f2c576cf250eeef369061697c
cjlwired <carlos.jose.lazo@gmail.com>
2013-12-29 21:35:15 -0500

    Add relevant example to sample spec for Rspec testing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,16 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), '/../app.rb')
 
-
 require 'sinatra'
 require 'rack/test'
 
-# setup test environment
+# Setup Test Environment
 set :environment, :test
 set :run, false
 set :raise_errors, true
 set :logging, false
 
 def app
-  AppName::App 
+  WBCR::App 
 end
 
 RSpec.configure do |config|
