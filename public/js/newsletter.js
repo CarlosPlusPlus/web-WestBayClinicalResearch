3e639e40d9e0e8178a756624ab4e0355d121ef15
cjlwired <carlos.jose.lazo@gmail.com>
2014-01-11 13:00:55 -0500

    Fix JS so that Newsletter formatting happens once Hubspot form is rendered.

--- public/js/newsletter.js
+++ public/js/newsletter.js
@@ -1,4 +1,4 @@
-$(function() {
+$(window).bind("load", function() {
 	var newsletter_form = $('#hsForm_0868cd36-34fb-4ae3-af9a-6aa7dc749b6a')
 	var submit_button  	= newsletter_form.find('.hs-button');
 	var submit_spacing 	= newsletter_form.find('.actions');
