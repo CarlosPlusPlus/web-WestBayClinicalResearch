10a607cfa81bf4ae9bb961cd33c82dc1f0c8a7bc
cjlwired <carlos.jose.lazo@gmail.com>
2014-01-13 23:37:52 -0500

    [ThankYou] Update styling on page and add space for image.

--- lib/helpers/presenter.rb
+++ lib/helpers/presenter.rb
@@ -1,17 +1,22 @@
 module Sinatra
   module PresenterHelper
     def thank_you_action(action)
-      respond = "<br>We will respond to you shortly."
+      respond = '<br><span class="tab">' + 
+                'We will respond to you shortly.' +
+                '</span>'
 
-      case action
-      when "contact","sponsor" then "contacting us!</b> " + respond
-      when "newsletter"        then "subscribing to our newsletter! </b>"
-      when "volunteer"         then "volunteering for a trial!</b> " + respond
-      end
+      '<b>Thank you for ' + 
+      (
+        case action
+        when 'contact','sponsor' then 'contacting us!</b>' + respond
+        when 'newsletter'        then 'subscribing to our newsletter!</b>'
+        when 'volunteer'         then 'volunteering for a trial!</b>' + respond
+        end
+      )
     end
 
     def thank_you_url(action)
-      "http://#{ domain }/thankyou?action=#{ action }"
+      'http://#{ domain }/thankyou?action=#{ action }'
     end
   end
 
