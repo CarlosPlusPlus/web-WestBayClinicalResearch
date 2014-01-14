d9b1bcd9b6168b488f5496d4d76c1762f348fd32
cjlwired <carlos.jose.lazo@gmail.com>
2014-01-13 22:58:33 -0500

    [ThankYou] Add in colored div with centered information.

--- lib/helpers/presenter.rb
+++ lib/helpers/presenter.rb
@@ -1,12 +1,12 @@
 module Sinatra
   module PresenterHelper
     def thank_you_action(action)
-      respond = "We will respond to you shortly."
+      respond = "<br>We will respond to you shortly."
 
       case action
-      when "contact","sponsor"  then "contacting us! " + respond
-      when "newsletter"         then "subscribing to our newsletter!"
-      when "volunteer"          then "volunteering for a trial! " + respond
+      when "contact","sponsor" then "contacting us!</b> " + respond
+      when "newsletter"        then "subscribing to our newsletter! </b>"
+      when "volunteer"         then "volunteering for a trial!</b> " + respond
       end
     end
 
