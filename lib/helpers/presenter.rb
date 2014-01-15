37b18a04e7a2ba4832f920270d1f22b66c30e275
cjlwired <carlos.jose.lazo@gmail.com>
2014-01-15 18:58:55 -0500

    Abstract social media group render to presenter helper module.

--- lib/helpers/presenter.rb
+++ lib/helpers/presenter.rb
@@ -1,5 +1,35 @@
 module Sinatra
   module PresenterHelper
+    
+    # Social Media Group
+    def email
+      '<a href="mailto:info@westbayclinicalresearch.com"' +
+        ' target="_blank" class="glyphicons e-mail"><i></i></a>'
+    end
+
+    def facebook
+      '<a href="#" class="glyphicons facebook"><i></i></a>'
+    end
+
+    def google_plus
+      '<a href="#" class="glyphicons google_plus"><i></i></a>'
+    end
+
+    def linked_in
+      '<a href="http://www.twitter.com/wbcresearch"' +
+        ' target="_blank" class="glyphicons twitter"><i></i></a>'
+    end
+
+    def twitter
+      '<a href="http://www.linkedin.com/company/west-bay-clinical-research"' + 
+        ' target="_blank" class="glyphicons linked_in"><i></i></a>'
+    end
+
+    def social_media_icons
+      email + facebook + google_plus + linked_in + twitter
+    end
+
+    # Thank You Page
     def thank_you_action(action)
       respond = '<br><span class="tab">' + 
                 'We will respond to you shortly.' +
