97ac4fcd599fdd34a72378df51eb29c91450fbc0
cjlwired <carlos.jose.lazo@gmail.com>
2014-02-03 23:33:15 -0500

    Move Sponsor a Trial under Learn More menu; update all actions accordingly.

--- app.rb
+++ app.rb
@@ -10,8 +10,8 @@ module WBCR
     set :javascripts, [:jquery, 'grids', 'newsletter']
 
     MAIN_MENU  = %w(blog contact faq)
-    LEARN_MORE = %w(about facility experience)
-    TRIALS     = %w(sponsor volunteer)
+    LEARN_MORE = %w(about facility experience sponsor)
+    TRIALS     = %w(volunteer)
 
     get '/' do
       @no_container = true
